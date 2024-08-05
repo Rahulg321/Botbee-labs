d5585872d5255b3446b94e8f5fd73be673f87b97
Rahul Gupta <rg5353070@gmail.com>
2024-08-05 14:42:16 +0530

    added a stagger animation

--- components/sections/home/SecondaryHero.tsx
+++ components/sections/home/SecondaryHero.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
+import { cn } from "@/lib/utils";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -25,7 +26,36 @@ const SecondaryHeroSection = () => {
       });
 
       t1.fromTo(".box", { opacity: 0 }, { opacity: 1, duration: 0.5 });
-      t1.fromTo(".car", { opacity: 0 }, { opacity: 1 }, "+=0.5");
+
+      t1.fromTo(
+        ".tech-row",
+        { opacity: 0 },
+        { opacity: 1, duration: 0.5, stagger: 0.3, ease: "power1.inOut" }
+      );
+      // t1.fromTo(
+      //   ".tech-row",
+      //   {
+      //     x: (index) => {
+      //       return index % 2 === 0
+      //         ? gsap.utils.random(600, 400)
+      //         : gsap.utils.random(-600, -400);
+      //     },
+      //   },
+      //   {
+      //     x: (index) => {
+      //       return index % 2 === 0
+      //         ? gsap.utils.random(-600, -400)
+      //         : gsap.utils.random(600, 400);
+      //     },
+      //     ease: "power1.inOut",
+      //     scrollTrigger: {
+      //       trigger: container.current,
+      //       start: "top bottom",
+      //       end: "bottom top",
+      //       scrub: 4,
+      //     },
+      //   }
+      // );
     },
     {
       scope: container,
@@ -33,7 +63,7 @@ const SecondaryHeroSection = () => {
   );
 
   return (
-    <section className="block-space-large bg-mainDark text-white">
+    <section className="block-space-large bg-mainDark text-white overflow-hidden">
       <div className="container" ref={container}>
         <div className="text-center box mb-6 md:mb-8 lg:mb-16 space-y-4">
           <h2>
@@ -45,10 +75,49 @@ const SecondaryHeroSection = () => {
             response time and many more.
           </span>
         </div>
-        <Image src={RegularCustomService} alt="" className="car" />
+
+        <div className="space-y-4">
+          <div className="tech-row flex">
+            <TextBox heading="How to handle massive Customer Support?" />
+            <BlankBox classname="flex-1" />
+            <TextBox heading="How can I elevate User Experience?" />
+          </div>
+          <div className="tech-row flex">
+            <BlankBox classname="flex-1" />
+            <TextBox heading="How can I save service cost?" />
+            <BlankBox classname="flex-1" />
+          </div>
+          <div className="tech-row flex">
+            <TextBox heading="How to save my valuable time from hiring process?" />
+            <BlankBox classname="flex-1" />
+            <TextBox heading="How can I attract more customers?" />
+          </div>
+          <div className="tech-row flex">
+            <BlankBox classname="flex-1" />
+            <TextBox heading="How can I increase engagement on my website?" />
+            <BlankBox classname="flex-1" />
+          </div>
+          <div className="tech-row flex">
+            <TextBox heading="How to serve quick results?" />
+            <BlankBox classname="flex-1" />
+            <TextBox heading="How can I automate the communication?" />
+          </div>
+        </div>
       </div>
     </section>
   );
 };
 
 export default SecondaryHeroSection;
+
+function TextBox({ heading }: { heading: string }) {
+  return (
+    <div className="p-4 rounded-full  border border-white">
+      <h5>{heading}</h5>
+    </div>
+  );
+}
+
+function BlankBox({ classname }: { classname?: string }) {
+  return <div className={cn("bg-[#1E1C2D] p-4 rounded-full", classname)}></div>;
+}
